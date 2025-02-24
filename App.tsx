43279ead328d2d744fdf2810537e972c2b6ccf72
rohanlinux2020@gmail.com <rohanlinux2020@gmail.com>
2025-02-25 00:18:52 +0530

    first

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+import "./global.css"
+
+
+const App = () => {
+  return (
+    <View>
+      <Text className='bg-red-600'>Rohan Tiwari khanpur</Text>
+    </View>
+  )
+}
+
+export default App
